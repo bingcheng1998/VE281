b290a6a704f75ef54bff2876def0703d2ec013d6
Bingcheng <bingcheng1998@qq.com>
2018-10-11 16:16:32 +0800

    data

--- Programming-Assignment/Programming-Assignment-2/answer/auto_gen.cpp
+++ Programming-Assignment/Programming-Assignment-2/answer/auto_gen.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <assert.h>
 #include <fstream>
+#include <direct.h>
 #include <math.h>
 using namespace std;
 int main(int argc, char *argv[]) {
@@ -11,14 +12,15 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < 100; ++i)
 	{
 		ostringstream path_stream;
-		path_stream<<i<<".txt";
+		path_stream<<"./data_file/"<<i<<".data";
 		oFile.open(path_stream.str());
 		double k = 1.0 + 2.0/100*(i);
 		int ek = pow(10, k);
 		cerr<<ek<<", ";
 
+		oFile<<0<<endl;
 		oFile<<ek<<endl;
-		oFile<<ek<<endl;
+		oFile<<0<<endl;
 		for (int i = 0; i < ek; ++i)
 		{
 			int k = mrand48();
--- Programming-Assignment/Programming-Assignment-2/answer/performance.cpp
+++ Programming-Assignment/Programming-Assignment-2/answer/performance.cpp
@@ -5,11 +5,11 @@
 #include <assert.h>
 #include <ctime>
 #include <fstream>
-#include "sort.h"
+#include "selection.h"
 
 using namespace std;
 
-#define SORT_WAY_SIZE 6
+#define SELECTION_WAY_SIZE 2
 #define file_num 100
 // #define SORT_DEBUG
 
@@ -22,10 +22,8 @@ void   Delay(int   time){
 	while(   clock()   -   now   <   time   ); 
 } 
 
-const string sortName[] = {
-	"bubble_sort", "insertion_sort", "selection_sort",
-	"merge_sort", "quick_sort_array", "quick_sort_in_place",
-	"ERROR_SORT_NAME"
+const string selectionName[] = {
+	"R selection","D selection","ERROR_SELECTION_Name"
 };
 
 bool safe_open_file(ifstream& i_file, string file_name){
@@ -42,38 +40,32 @@ bool safe_open_file(ifstream& i_file, string file_name){
 }
 
 static int int_size[] = {
-	10, 10, 10, 11, 12, 12, 13, 13, 14, 15, 15, 16, 17, 18, 19, 19, 20, 21, 22, 23, 25, 26, 27, 28, 30, 31, 33, 34, 36, 38, 39, 41, 43, 45, 47, 50, 52, 54, 57, 60, 63, 66, 69, 72, 75, 79, 83, 87, 91, 95, 100, 104, 109, 114, 120, 125, 131, 138, 144, 151, 158, 165, 173, 181, 190, 199, 208, 218, 229, 239, 251, 263, 275, 288, 301, 316, 331, 346, 363, 380, 398, 416, 436, 457, 478, 501, 524, 549, 575, 602, 630, 660, 691, 724, 758, 794, 831, 870, 912, 954};
+	10, 10, 10, 11, 12, 12, 13, 13, 14, 15, 15, 16, 17, 18, 19, 19, 20, 21, 22, 23, 25, 26, 27, 28, 30, 31, 33, 34, 36, 38, 39, 41, 43, 45, 47, 50, 52, 54, 57, 60, 63, 66, 69, 72, 75, 79, 83, 87, 91, 95, 100, 104, 109, 114, 120, 125, 131, 138, 144, 151, 158, 165, 173, 181, 190, 199, 208, 218, 229, 239, 251, 263, 275, 288, 301, 316, 331, 346, 363, 380, 398, 416, 436, 457, 478, 501, 524, 549, 575, 602, 630, 660, 691, 724, 758, 794, 831, 870, 912, 954 };
 
 bool jump_j[] = {false, false,false,false,false,false};
 
 int main(int argc, char *argv[]) {
-    void (*const fn[SORT_WAY_SIZE])(int*, const int) = {
-        bubble_sort,
-        insertion_sort,
-        selection_sort,
-        merge_sort,
-        quick_sort_array,
-        quick_sort_in_place
+	int (*const fn[SELECTION_WAY_SIZE])(int*, const int, const int) = {
+        random_selection,
+        deterministric_selection
     };
 
 	clock_t start, end;
 	ofstream outFile;
 	outFile.open("data.csv", ios::out);
-	outFile << "size"<<","<<"bubble_sort"<<","
-		<<"insertion_sort"<<","<<"selection_sort"<<
-		","<<"merge_sort"<<","<<"quick_sort_array"<<
-		"," <<"quick_sort_in_place"<<","<<endl;
+	outFile << "size"<<","<<selectionName[0]<<","<<selectionName[1]<<","<<endl;
 	for (int j = 0; j < file_num; ++j)
 	{
 		ifstream iFile;
 		
 		
 		ostringstream path_stream;
-		path_stream<<j<<".txt";
+		path_stream<<"./data_file/"<<j<<".data";
 		safe_open_file(iFile, path_stream.str());
 		int lines;
 		iFile >> lines;
 		iFile >> lines;
+
 		
 		int *arr = new int[lines];
 		int baz;
@@ -83,11 +75,11 @@ int main(int argc, char *argv[]) {
 		 	arr[i] = baz;
 		 } 
 		outFile << int_size[j]<<",";
-		for (int i = 0; i < SORT_WAY_SIZE; ++i)
+		for (int i = 0; i < SELECTION_WAY_SIZE; ++i)
 		{
 			if(jump_j[i] == true){
 				outFile << ""<<",";
-				cerr<<"jump "<<sortName[i]<<" with "<<int_size[j]<<" size!"<<endl;
+				cerr<<"jump "<<selectionName[i]<<" with "<<int_size[j]<<" size!"<<endl;
 				continue;
 			}
 			if(int_size[j] < 1000){
@@ -99,9 +91,9 @@ int main(int argc, char *argv[]) {
 			memset(arr_copy,0, lines*sizeof(int));
 			memcpy(arr_copy,arr, lines*sizeof(int));
 			start = clock();
-			fn[i](arr_copy, lines);
+			fn[i](arr_copy, lines, 0);
 			end = clock();
-			cout<<"Sort algorithm is ["<<sortName[i]<<"],";
+			cout<<"Sort algorithm is ["<<selectionName[i]<<"],";
 			double time_run =  (double)(end - start) / CLOCKS_PER_SEC;
 			cout << "Running time: " <<time_run<< endl;
 			if (time_run >= 24.0)

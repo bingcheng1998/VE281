6f8c42ebad25740a89c2829f8a332126cf40520c
Bingcheng <bingcheng1998@qq.com>
2018-11-05 23:41:09 +0800

    fib heap v2

--- Programming-Assignment/Programming-Assignment-3/Project 3 code/fib_heap.h
+++ Programming-Assignment/Programming-Assignment-3/Project 3 code/fib_heap.h
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <list>
 #include "priority_queue.h"
 
 // OVERVIEW: A specialized version of the 'heap' ADT implemented as a 
@@ -59,13 +60,16 @@ private:
   {
       TYPE key;
       typename std::list<node> child;
-      node parent;
+      // node parent;
       int degree=0;
   };
   typename std::list<node> root;
   typename std::list<node>::iterator min;
   int n=0;
   TYPE empty_fib=TYPE();
+
+  // virtual void Consolidate();
+  // virtual void Fibonacci_Heap_Link(x, y);
 };
 
 // Add the definitions of the member functions here. Please refer to
@@ -82,8 +86,13 @@ fib_heap<TYPE,COMP>::fib_heap(COMP comp)
 {
     compare=comp;
     // Fill in the remaining lines if you need.
-    min=NULL;
+    min = root.begin();
     n=0;
+}
+
+template<typename TYPE,typename COMP>
+fib_heap<TYPE,COMP>::~fib_heap()
+{
 }
 
 // Fibonacci_Heap_Enqueue(H, key) {
@@ -112,41 +121,51 @@ void fib_heap<TYPE,COMP>::enqueue(const TYPE&val)
     node x;
     x.key=val;
     x.degree=0;
-    x.parent = NULL;
-    x.child = NULL;
-    if(min == NULL)
+    // x.parent = NULL;
+    // x.child = NULL;
+    if(root.empty()==true)
     {
         root.push_back(x);
-        H_min=x;
+        min=root.begin();
     }
     else
     {
-        // typename std::list<fib_node>::iterator it;
+        // typename std::list<node>::iterator it;
         // it=root.insert(root.end(),n);
-        root.insert(root.end(),n);
-        if(compare(x.key,(*H_min).key))
+        root.insert(root.end(),x);
+        if(compare(x.key,(*min).key))
         {
-            // H_min=it;
-          H_min=x;
+            // min=it;
+          min=root.begin();
         }
     }
     n++;
 };
 
-Fibonacci_Heap_Dequeue_Min(H) { z = H.min;
-  if(z != NULL) {
-     for(each child x of z) {
-       add x to the root list of H;
-       x.parent = NULL;
-     }
-     remove z from the root list of H;
-     H.n = H.n – 1;
-     if(H.n == 0) H.min = NULL;
+// Fibonacci_Heap_Dequeue_Min(H) { z = H.min;
+//   if(z != NULL) {
+//      for(each child x of z) {
+//        add x to the root list of H;
+//        x.parent = NULL;
+//      }
+//      remove z from the root list of H;
+//      H.n = H.n – 1;
+//      if(H.n == 0) H.min = NULL;
    
-  else Consolidate(H);
-    }
-  return z; 
-}
+//   else Consolidate(H);
+//     }
+//   return z; 
+// }
+
+// template<typename TYPE,typename COMP>
+// TYPE fib_heap<TYPE,COMP>::dequeue_min()
+// {
+//     if(root.empty()==true)
+//     {
+//         return empty_fib;
+//     }
+
+// };
 
 template<typename TYPE,typename COMP>
 TYPE fib_heap<TYPE,COMP>::dequeue_min()
@@ -155,10 +174,13 @@ TYPE fib_heap<TYPE,COMP>::dequeue_min()
     {
         return empty_fib;
     }
-    fib_node z;
+    
+//    std::cout<<std::endl;
+    
+    node z;
     z=*min;
-    typename std::list<fib_node>::iterator temp;
-    if(H_min!=root.end())
+    typename std::list<node>::iterator temp;
+    if(min!=root.end())
     {
         temp=z.child.begin();
         while(temp!=z.child.end())
@@ -166,30 +188,58 @@ TYPE fib_heap<TYPE,COMP>::dequeue_min()
             root.push_back(*temp);
             temp=z.child.erase(temp);
         }
-        H_min=root.erase(H_min);
-        H_n--;
-        if(H_n==0)
+        min=root.erase(min);
+        n--;
+        if(n==0)
         {
-            H_min=root.end();
+            min=root.end();
         }
         else
         {
-            int size=int((log(H_n))/(log((1+sqrt(5))/2)))+1;
-            typename std::list<fib_node>::iterator A[size];
+//=-------------------------------------------------------------
+            int size=int((log(n))/(log((1+sqrt(5))/2)))+1;
+            typename std::list<node>::iterator A[size];
             for(int i=0;i<size;++i)
             {
                 A[i]=root.end();
             }
-            typename std::list<fib_node>::iterator y;
+//            typename std::list<node>::iterator x;
+            typename std::list<node>::iterator y;
             int d=0;
-            typename std::list<fib_node>::iterator it;
+            typename std::list<node>::iterator it;
             for(it=root.begin();it!=root.end();++it)
             {
+                
+//                std::cout<<"item in A ";
+//                for(int i=0;i<size;++i)
+//                {
+//                    if(A[i]==root.end()){
+//                        std::cout<<"NULL"<<" ";
+//                    }
+//                    else{
+//                        std::cout<<(*A[i]).val<<" ";
+//                    }
+//                }
+//                std::cout<<std::endl;
+                
                 d=(*it).degree;
                 while(A[d]!=root.end())
                 {
                     y=A[d];
-                    if(compare((*y).val,(*it).val))
+                    
+//                    std::cout<<"item in A ";
+//                    for(int i=0;i<size;++i)
+//                    {
+//                        if(A[i]==root.end()){
+//                            std::cout<<"NULL"<<" ";
+//                        }
+//                        else{
+//                            std::cout<<(*A[i]).val<<" ";
+//                        }
+//                    }
+//                    std::cout<<std::endl;
+                    
+                    if(compare((*y).key,(*it).key))
                     {
                         root.insert(y,*it);
                         root.insert(it,*y);
@@ -198,82 +248,133 @@ TYPE fib_heap<TYPE,COMP>::dequeue_min()
                         it--;
                         y--;
                     }
+                    
+//                    std::cout<<"item in A ";
+//                    for(int i=0;i<size;++i)
+//                    {
+//                        if(A[i]==root.end()){
+//                            std::cout<<"NULL"<<" ";
+//                        }
+//                        else{
+//                            std::cout<<(*A[i]).val<<" ";
+//                        }
+//                    }
+//                    std::cout<<std::endl;
+//=-------------------------------------------------------------
                     (*it).child.push_back((*y));
                     y=root.erase(y);
                     (*it).degree++;
                     A[d]=root.end();
+//=-------------------------------------------------------------
+//                    std::cout<<"item in A ";
+//                    for(int i=0;i<size;++i)
+//                    {
+//                        if(A[i]==root.end()){
+//                            std::cout<<"NULL"<<" ";
+//                        }
+//                        else{
+//                            std::cout<<(*A[i]).val<<" ";
+//                        }
+//                    }
+//                    std::cout<<std::endl;
+//                    typename std::list<node>::iterator ttt;
+//                    int testt=0;
+//                    for(ttt=root.begin();ttt!=root.end();++ttt)
+//                    {
+//                        printf("The %d item in rootlost is %d\n",testt,(*ttt).val);
+//                        typename std::list<node>::iterator tttt;
+//                        for(tttt=(*ttt).child.begin();tttt!=(*ttt).child.end();++tttt)
+//                        {
+//                            std::cout<<(*tttt).val<<" ";
+//                        }
+//                        testt++;
+//                        std::cout<<std::endl;
+//                    }
+//                    std::cout<<"END"<<std::endl;
+                    
                     d++;
                 }
                 A[d]=it;
             }
-            H_min=root.end();
+            min=root.end();
             for(int i=0;i<size;++i)
             {
                 if(A[i]!=root.end())
                 {
-                    if(H_min==root.end())
+                    if(min==root.end())
                     {
-                        H_min=A[i];
+                        min=A[i];
                     }
                     else
                     {
-                        if(compare((*A[i]).val,(*H_min).val))
+                        if(compare((*A[i]).key,(*min).key))
                         {
-                            H_min=A[i];
+                            min=A[i];
                         }
                     }
                 }
             }
         }
     }
-    return z.val;
+    return z.key;
 };
 
 
 
 
-Consolidate(H) {
-let A[0..D(H.n)] be a new array; // D(N) is the maximum degree of
-  for(i = 0 to D(H.n))
-  A[i] = NULL;
-  // any node in a Fibonacci heap of N nodes
-  for(each node w in the root list of H) {
-    x = w;
-    d = x.degree;
-    while(A[d] != NULL) {
-       y = A[d];
-       if(x.key > y.key) exchange x with y;
-       Fibonacci_Heap_Link(H, y, x);
-       A[d] = NULL;
-       d = d + 1;
-    }
-    A[d] = x;
-  }
-  H.min = NULL;
-  for(i = 0 to D(H.n)) {
-    if(A[i] != NULL) {
-      if(H.min == NULL) {
-        create a root list for H only containing A[i];
-        H.min = A[i];
-      }
-     else {
-        add A[i] to the root list of H;
-        if(A[i].key < H.min.key)
-                 H.min = A[i];
-        }
-    }
-  }
-}
+// Consolidate(H) {
+// let A[0..D(H.n)] be a new array; 
+//   // D(N) is the maximum degree of
+//   // any node in a Fibonacci heap of N nodes
+//   for(i = 0 to D(H.n))
+//   A[i] = NULL;
+//   for(each node w in the root list of H) {
+//     x = w;
+//     d = x.degree;
+//     while(A[d] != NULL) {
+//        y = A[d];
+//        if(x.key > y.key) exchange x with y;
+//        Fibonacci_Heap_Link(H, y, x);
+//        A[d] = NULL;
+//        d = d + 1;
+//     }
+//     A[d] = x;
+//   }
+//   H.min = NULL;
+//   for(i = 0 to D(H.n)) {
+//     if(A[i] != NULL) {
+//       if(H.min == NULL) {
+//         create a root list for H only containing A[i];
+//         H.min = A[i];
+//       }
+//      else {
+//         add A[i] to the root list of H;
+//         if(A[i].key < H.min.key)
+//                  H.min = A[i];
+//         }
+//     }
+//   }
+// }
+
+// template<typename TYPE,typename COMP>
+// fib_heap<TYPE,COMP>::Consolidate(){
+
+// }
 
 
 
-Fibonacci_Heap_Link(H, y, x) {
-  remove y from the root list of H;
-  make y a child of x;
-  x.degree = x.degree + 1;
-  // y.mark = FALSE; // This statement is commented out, because you
-                       // do not need it in this project.
-}
+// Fibonacci_Heap_Link(H, y, x) {
+//   remove y from the root list of H;
+//   make y a child of x;
+//   x.degree = x.degree + 1;
+//   // y.mark = FALSE; // This statement is commented out, because you
+//                        // do not need it in this project.
+// }
+
+// template<typename TYPE,typename COMP>
+// fib_heap<TYPE,COMP>::Fibonacci_Heap_Link(x, y){
+
+// }
 
 
 // Fibonacci_Heap_Get_Min(H) { 

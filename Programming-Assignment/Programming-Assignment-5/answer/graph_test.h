64ec49d6834cbee0e1ba0d4d1276c1c353090cb5
Bingcheng <bingcheng1998@qq.com>
2018-11-30 18:50:01 +0800

    v1.2

--- Programming-Assignment/Programming-Assignment-5/answer/graph_test.h
+++ Programming-Assignment/Programming-Assignment-5/answer/graph_test.h
@@ -16,42 +16,41 @@ struct Node;
 struct Edge;
 
 struct Node {
-  int code = 0;
-  int D = 0;
-  list<Edge> adjacent_list;
-  list<Edge> undirected_list;
-  int degree = 0;
+    int code = 0;
+    int D = 0;
+    list<Edge> adjacent_list;
+    list<Edge> undirected_list;
+    int degree = 0;
 };
 
 struct Edge {
-  Node *end_node;
-  int weight = 0;
+    Node *end_node;
+    int weight = 0;
 };
 
 struct Edge_comp {
-  bool operator()(const Node *a, const Node *b) const {
-    return a->code < b->code;
-  }
+    bool operator()(const Node *a, const Node *b) const {
+        return a->code < b->code;
+    }
 };
 
 struct Graph {
-  vector<Node *> NodeAll_vec; // compare by in_degree
-  multimap<Node *, Edge, Edge_comp> EdgeAll_map; // compare by code
-  multimap<Node *, Edge, Edge_comp> Undirected_EdgeAll_map; //compare by code
+    vector<Node *> node_vec; // compare by in_degree
+    multimap<Node *, Edge, Edge_comp> edge_map; // compare by code
+    multimap<Node *, Edge, Edge_comp> undirected_edge_map; //compare by code
 };
 
 struct comp_D {
-  bool operator()(const Node *a, const Node *b) const {
-    return a->D < b->D;
-  }
+    bool operator()(const Node *a, const Node *b) const {
+        return a->D < b->D;
+    }
 };
 
 bool comp_node_degree(const Node *a, const Node *b);
+
 bool comp_node_code(const Node *a, const Node *b);
 
 void tell_DAG(Graph graph);
-
-// void Shortest_Path(Graph graph, int source_node_code, int destination_node_code);
 
 void calculate_MST(Graph graph);
 
--- Programming-Assignment/Programming-Assignment-5/answer/main.cpp
+++ Programming-Assignment/Programming-Assignment-5/answer/main.cpp
@@ -4,44 +4,46 @@
 using namespace std;
 
 int main() {
-  int node_num;
-  cin >> node_num ;
-  Graph graph = Graph();
-  for (int i = 0; i < node_num; i++) {
-    graph.NodeAll_vec.push_back(new Node);
-  }
-  getchar();
-  stringstream stream_read;
-  while (!cin.eof()) {
-    string str;
-    getline(cin, str);
-    if (str.empty()) {
-      break;
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int node_num;
+    cin >> node_num;
+    Graph graph = Graph();
+    for (int i = 0; i < node_num; i++) {
+        graph.node_vec.push_back(new Node);
     }
-    stream_read.clear();
-    stream_read.str(str);
-    int node_start_code, node_end_code;
-    Edge edge_temp;
-    Edge undirected_edge_temp_1;
-    Edge undirected_edge_temp_2;
-    stream_read >> node_start_code >> node_end_code >> edge_temp.weight;
-    undirected_edge_temp_1.weight = undirected_edge_temp_2.weight = edge_temp.weight;
-    graph.NodeAll_vec[node_start_code]->code = node_start_code;
-    graph.NodeAll_vec[node_end_code]->code = node_end_code;
-    graph.NodeAll_vec[node_end_code]->degree++;
-    graph.NodeAll_vec[node_start_code]->D = graph.NodeAll_vec[node_end_code]->D = 0;
-    edge_temp.end_node = graph.NodeAll_vec[node_end_code];
-    undirected_edge_temp_1.end_node = graph.NodeAll_vec[node_end_code];
-    undirected_edge_temp_2.end_node = graph.NodeAll_vec[node_start_code];
-    graph.NodeAll_vec[node_start_code]->adjacent_list.push_back(edge_temp);
-    graph.NodeAll_vec[node_start_code]->undirected_list.push_back(undirected_edge_temp_1);
-    graph.NodeAll_vec[node_end_code]->undirected_list.push_back(undirected_edge_temp_2);
-    graph.EdgeAll_map.insert(make_pair(graph.NodeAll_vec[node_start_code], edge_temp));
-    graph.Undirected_EdgeAll_map.insert(make_pair(graph.NodeAll_vec[node_start_code], undirected_edge_temp_1));
-    graph.Undirected_EdgeAll_map.insert(make_pair(graph.NodeAll_vec[node_end_code], undirected_edge_temp_2));
-  }
+    getchar();
+    stringstream stream_read;
+    while (!cin.eof()) {
+        string str;
+        getline(cin, str);
+        if (str.empty()) {
+            break;
+        }
+        stream_read.clear();
+        stream_read.str(str);
+        int node_start_code, node_end_code;
+        Edge edge_temp;
+        Edge undirected_edge_temp_1;
+        Edge undirected_edge_temp_2;
+        stream_read >> node_start_code >> node_end_code >> edge_temp.weight;
+        undirected_edge_temp_1.weight = undirected_edge_temp_2.weight = edge_temp.weight;
+        graph.node_vec[node_start_code]->code = node_start_code;
+        graph.node_vec[node_end_code]->code = node_end_code;
+        graph.node_vec[node_end_code]->degree++;
+        graph.node_vec[node_start_code]->D = graph.node_vec[node_end_code]->D = 0;
+        edge_temp.end_node = graph.node_vec[node_end_code];
+        undirected_edge_temp_1.end_node = graph.node_vec[node_end_code];
+        undirected_edge_temp_2.end_node = graph.node_vec[node_start_code];
+        graph.node_vec[node_start_code]->adjacent_list.push_back(edge_temp);
+        graph.node_vec[node_start_code]->undirected_list.push_back(undirected_edge_temp_1);
+        graph.node_vec[node_end_code]->undirected_list.push_back(undirected_edge_temp_2);
+        graph.edge_map.insert(make_pair(graph.node_vec[node_start_code], edge_temp));
+        graph.undirected_edge_map.insert(make_pair(graph.node_vec[node_start_code], undirected_edge_temp_1));
+        graph.undirected_edge_map.insert(make_pair(graph.node_vec[node_end_code], undirected_edge_temp_2));
+    }
     tell_DAG(graph);
     calculate_MST(graph);
-  return 0;
+    return 0;
 }
 

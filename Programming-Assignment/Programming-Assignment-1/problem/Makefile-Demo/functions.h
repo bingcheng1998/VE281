128b5b414d60bf2d5c044ced1c5756c26866b832
Bingcheng <bingcheng1998@qq.com>
2018-12-08 12:03:03 +0800

    delete some files

--- Programming-Assignment/Programming-Assignment-1/problem/Makefile-Demo/functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-void print_hello();
-int factorial(int n);
-
-#endif
--- Programming-Assignment/Programming-Assignment-5/answer/test_files/cut.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for((i=0;i<=20;i++))
-do   
-	echo $i.out 被修改\! 
-	sed -i '' '1d' $i.out
-done 
